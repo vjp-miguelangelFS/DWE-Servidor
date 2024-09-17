c955e69602342d282468fb9458e8f55c8ce612dc
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-17 20:15:02 +0200

    .

--- EjerciciosUT03/cadenas1.php
+++ EjerciciosUT03/cadenas1.php
@@ -11,26 +11,33 @@ $nombre = "anaMArial"
 </head>
 <body>
     <?php 
+    // 2
     print $nombre . "<br>";
+    // 3
     print strlen($nombre) . "<br>";
+    // 4
     print strtoupper($nombre) . "<br>";
+    // 5
     print strtolower($nombre) . "<br>";
+    // 6
     print substr_count($nombre,strtoupper('a')) . "<br>";
+    // 7
     print ord($nombre) . "<br>";
-
+    // 8
     print strpos($nombre,'a') . "<br>";
+    // 9
     print strrpos($nombre,'a') . "<br>";
-
+    // 19
     print str_ireplace('a',0,$nombre) . "<br>";
-
+    // 11
     if(str_starts_with($nombre,'al') == true){
         print 'El nombre empieza por al' . "<br>";
     }else{
         print 'El nombre no empieza por al' . "<br>";
     }
-
+    // 12
     print trim(rtrim($nombre)) . "<br>";
-
+    // 13
     print ucfirst(strtolower($nombre));
     ?>
 </body>
