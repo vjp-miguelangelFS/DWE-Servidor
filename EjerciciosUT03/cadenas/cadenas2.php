68ec8d00eff4899d720ba84d3b347460e6860bec
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-26 18:03:38 +0200

    .

--- EjerciciosUT03/cadenas/cadenas2.php
+++ EjerciciosUT03/cadenas/cadenas2.php
@@ -10,53 +10,57 @@
 
 <body>
     <?php
-    // 1
+    // 1 Muestra el valor de un parametro llamado name
     $name = $_GET['name'] ?? 'Miguel';
     $name = trim($name, '/');
-    print $name;
-    echo "<br>";
+    print 'Nombre introducido por parametro: '.$name;
+    echo "<br><br>";
 
-    // 2
+    // 2 Muestra la longuitud del parametro name
 
-    print strlen($name);
-    echo "<br>";
+    print 'Longuitud del parametro name: '.strlen($name);
+    echo "<br><br>";
 
-    // 3
+    // 3 Muestra el parametro name en mayuscula
 
-    print strtoupper($name);
-    echo "<br>";
+    print 'Parametro no en mayuscula: '.strtoupper($name);
+    echo "<br><br>";
 
-    // 4
+    // 4 Muestra el parametro name en minuscula
 
-    print strtolower($name);
-    echo "<br>";
+    print 'Parametro no en minuscula: '.strtolower($name);
+    echo "<br><br>";
 
-    // 5
+    // 5 Muestra si el nombre empieza por el prefijo introducido por parametro
 
-    $prefix = $_GET['prefix'];
-
-    if (strpos($name, $prefix) === 0) {
-        echo 'El nombre empieza por el prefijo introducido';
-    } else {
-        echo 'El nombre no empieza por el prefijo introducido';
+    $prefix = $_GET['prefix'] ?? null;
+    if($prefix != null){
+        if (strpos($name, $prefix) === 0) {
+            echo 'El nombre empieza por el prefijo introducido';
+        } else {
+            echo 'El nombre no empieza por el prefijo introducido';
+        }
+    }else{
+        print 'No se ha paso el parametro';
     }
-    echo '<br>';
-    // 6
+    
+    echo '<br><br>';
+    // 6 Muestra el numero de veces que aparece la letra e
 
-    print substr_count(strtolower($name), "e");
-    echo '<br>';
+    print 'Numero de veces que aparece la letra e son: '.substr_count(strtolower($name), "e");
+    echo '<br><br>';
 
-    // 7
+    // 7 Muestra la posición de la primera a
 
     if (strpos($name, 'a')) {
-        print strpos($name, 'a');
+        print 'Posición de la primera a: '.strpos($name, 'a');
     } else {
         echo 'En este texto no hay ninguna a';
     }
-    echo '<br>';
-    // 8
+    echo '<br><br>';
+    // 8 Muestra el parametro name sustituyendo las letras o por 0
 
-    print str_ireplace('o', 0, $name)
+    print 'Parametro name sustituyendo la letra o por 0'.str_ireplace('o', 0, $name)
     ?>
 
 </body>
