2c17be12b58bfbd96461cd21878342a69ebf2b36
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-26 17:51:25 +0200

    .

--- EjerciciosUT03/cadenas/cadenas1.php
+++ EjerciciosUT03/cadenas/cadenas1.php
@@ -14,37 +14,51 @@
     $nombre = "anaMArial";
     // 2 Muestra la variable
     print $nombre . "<br>";
+    echo ('<br>');
     // 3 Muestra la longuitud de la variaple
-    print strlen($nombre) . "<br>";
+    print 'La longuitud de la variable es: '.strlen($nombre) . "<br>";
+    echo ('<br>');
     // 4 Trasforma la variable a mayuscula
-    print strtoupper($nombre) . "<br>";
+    print 'Variable en mayuscula: '.strtoupper($nombre) . "<br>";
+    echo ('<br>');
     // 5 Trasforma la variable a minuscula
-    print strtolower($nombre) . "<br>";
+    print 'Variable en minuscula: '.strtolower($nombre) . "<br>";
+    echo ('<br>');
     // 6 Muestra el numero de veces que aparece la letra a
-    print substr_count($nombre, strtoupper('a')) . "<br>";
+    print 'La cantidad de veces que aparece a son: '.substr_count($nombre, strtoupper('a')) . "<br>";
+    echo ('<br>');
     // 7 Muestra el codigo ASCIi de la primera letra
-    print ord($nombre) . "<br>";
+    print 'El codigo ascii de la primera letra es: '.ord($nombre) . "<br>";
+    echo ('<br>');
     // 8 Muestra la posición de la primera a
     if(strpos($nombre, 'a') != ''){
-        print strpos($nombre, 'a');
+        print 'La primera a ocupa la posición: '.strpos($nombre, 'a');
     }else{
-        print (-1);
+        print (-1).' No contiene ninguna a';
     };
-    echo ('<br>');
+    echo ('<br><br>');
+    
     // 9 Lo mismo que el 8 pero con la ultima
-    print strrpos($nombre, 'a') . "<br>";
+    if(strrpos($nombre, 'a') != ''){
+        print 'La ultima a ocupa la posición: '.strrpos($nombre, 'a');
+    }else{
+        print (-1).' No contiene ninguna a';
+    };
+    echo ('<br><br>');
     // 10 Sustituye las letras a por 0
-    print str_ireplace('a', 0, $nombre) . "<br>";
+    print 'Variable remplazando las a por 0'.str_ireplace('a', 0, $nombre) . "<br>";
     // 11 Indica si el nombre empieza por al o no
     if (str_starts_with($nombre, 'al') == true) {
         print 'El nombre empieza por al' . "<br>";
     } else {
         print 'El nombre no empieza por al' . "<br>";
     }
+    echo ('<br>');
     // 12 Elimina los espacios al principio y al final 
     print trim(rtrim($nombre)) . "<br>";
+    echo ('<br>');
     // 13 Muestra la variable con la primera letra en mayuscula y las demas en minuscula
-    print ucfirst(strtolower($nombre));
+    print 'Variable con la primera mayuscula y las demas en minuscula'.ucfirst(strtolower($nombre));
     ?>
 </body>
 
