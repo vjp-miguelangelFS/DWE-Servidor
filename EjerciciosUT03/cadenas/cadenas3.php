a84e708ff37ab58ed220f75f6aa6eb8b6db7e16b
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-09-26 18:04:58 +0200

    .

--- EjerciciosUT03/cadenas/cadenas3.php
+++ EjerciciosUT03/cadenas/cadenas3.php
@@ -13,10 +13,13 @@
 
     $url = 'http://username:password@hostname:9090/path?arg=value#anchor';
 
-
+    // 1 Protocolo http
     print "1- Protocolo: " . parse_url($url, PHP_URL_SCHEME) . "<br>";
+    // 2 Nombre de usuario
     print "2- Username: " . parse_url($url, PHP_URL_USER) . "<br>";
+    // 3 Path de la url
     print "3- Path: " . parse_url($url, PHP_URL_PATH) . "<br>";
+    // 4 El querystring
     print "4- Querystring: " . parse_url($url, PHP_URL_QUERY) . "<br>";
 
     ?>
