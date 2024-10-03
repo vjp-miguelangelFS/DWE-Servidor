3c6862423b247047bb88ca1b9e99852b67552d05
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-10-03 19:30:40 +0200

    .

--- EjerciciosUT04/practica5/canciones.inc.php
+++ EjerciciosUT04/practica5/canciones.inc.php
@@ -6,8 +6,17 @@ function buscarCancion(...$busqueda)
 {
 
     $canciones = [
-        ['titulo' => 'Hola', 'album' => 'ewqewq', 'genero' => 'Pop'],
-        ['titulo' => 'Hola2', 'album' => 'ewqewq2', 'genero' => 'Jazz']
+        ['titulo' => 'Take On Me', 'album' => 'Hunting High and Low', 'genero' => 'Pop'],
+        ['titulo' => 'Dancing Queen', 'album' => 'Arrival', 'genero' => 'Pop'],
+
+        ['titulo' => 'Thunder Road', 'album' => 'Born to Run', 'genero' => 'Rock'],
+        ['titulo' => 'Highway Star', 'album' => 'Machine Head', 'genero' => 'Rock'],
+
+        ['titulo' => 'Take Five', 'album' => 'Time Out', 'genero' => 'Jazz'],
+        ['titulo' => 'My Baby Just Cares for Me', 'album' => 'Little Girl Blue', 'genero' => 'Jazz'],
+
+        ['titulo' => 'Dancing Queen', 'album' => 'Arrival', 'genero' => 'Blues'],
+        ['titulo' => 'Dancing Queen', 'album' => 'Arrival', 'genero' => 'Blues'],
     ];
 
     return $canciones;
@@ -19,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $buscarEn = $_POST['radio1'];
     $genero = $_POST['genero'];
 
-    
+
 }
 
 ?>
